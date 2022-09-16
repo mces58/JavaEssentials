2c94ca28792f95154f1ad0f6dfed6f4189d22ffc
can <72516854+mces58@users.noreply.github.com>
2022-09-16 15:21:46 +0300

    commit

---
+++ 01-Spring/028-Inheritance/src/com/spring/Database.java
@@ -0,0 +1,43 @@
+package com.spring;
+
+public class Database {
+
+	private String url;
+	private String userName;
+	private String password;
+
+	public Database() {
+
+	}
+
+	public Database(String url, String userName, String password) {
+		this.url = url;
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ 01-Spring/028-Inheritance/src/com/spring/Main.java
@@ -0,0 +1,21 @@
+package com.spring;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// hangi database ile calismak istiyorsan o bean'e ait id degerini kullan
+		Database database = context.getBean("oracle", Database.class);
+
+		System.out.println("url: " + database.getUrl());
+		System.out.println("userName: " + database.getUserName());
+		System.out.println("password: " + database.getPassword());
+
+		context.close();
+	}
+}
