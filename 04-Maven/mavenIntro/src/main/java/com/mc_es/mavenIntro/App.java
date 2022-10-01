919788d9719bd29e9e0eb3c5beb2ec29d44f2195
can <72516854+mces58@users.noreply.github.com>
2022-10-01 14:46:49 +0300

    commit

---
+++ 04-Maven/mavenIntro/src/main/java/com/mc_es/mavenIntro/App.java
@@ -0,0 +1,13 @@
+package com.mc_es.mavenIntro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
