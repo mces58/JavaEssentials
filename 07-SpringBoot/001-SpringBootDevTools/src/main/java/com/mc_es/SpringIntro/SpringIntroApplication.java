df6723ae2358d608c0bfa6e2e6661219013a8273
can <72516854+mces58@users.noreply.github.com>
2022-10-17 15:16:48 +0300

    commit

---
+++ 07-SpringBoot/001-SpringBootDevTools/src/main/java/com/mc_es/SpringIntro/SpringIntroApplication.java
@@ -0,0 +1,13 @@
+package com.mc_es.SpringIntro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroApplication.class, args);
+	}
+
+}
